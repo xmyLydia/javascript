778886921de20b6523236f680a398999dc664a02
mxie <836728902@qq.com>
2019-11-17 17:35:30 +0200

    ES6 new features

---
+++ ES6/script.js
@@ -0,0 +1,45 @@
+//let and const
+//ES5
+/*var name5 = 'Jane';
+var age5 = 23;
+name5 = 'Janny';
+console.log(name5);
+//ES6
+const name6 = 'Jane Smith';
+let age6 = 23;
+//name6 = 'Janny6';
+//console.log(name6);
+
+//ES5
+function driversLicense(passedTest){
+    if(passedTest){
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+    }
+    console.log(firstName+','+yearOfBirth+' is allowed');
+
+}
+driversLicense(true);
+//ES6
+function driversLicense6(passedTest){
+
+    let firstName;
+    const yearOfBirth = 1990;
+    if(passedTest){
+        firstName = 'John'; 
+    } 
+          console.log(firstName+','+yearOfBirth+' is allowed');
+}
+driversLicense6(true);
+let i = 23;
+for(let i=0;i<5;i++){
+    console.log(i);
+}
+ console.log(i);
+ */
+{
+    const a = 1;
+    let b = 2;
+}
+console.log(a+b);
+
