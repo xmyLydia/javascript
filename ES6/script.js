1031aab26a710f6b02f8a03700c450204d9e3c97
mxie <836728902@qq.com>
2019-11-19 22:15:21 +0200

    Arrow function

--- ES6/script.js
+++ ES6/script.js
@@ -36,10 +36,47 @@ for(let i=0;i<5;i++){
     console.log(i);
 }
  console.log(i);
- */
+ 
 {
     const a = 1;
     let b = 2;
 }
 console.log(a+b);
 
+let firstName = 'Henry';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+function calcAge(year){
+    return 2019 - year;
+}
+//ES5
+console.log('This is '+ firstName + ' '+lastName+' .He was born in '+ yearOfBirth
+           +' today he is '+calcAge(yearOfBirth)+' years old.');
+//ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}
+ , today he is ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('H'));//start with
+console.log(n.endsWith('th'));//ends with
+console.log(n.includes('mi'));//if it is in the middle
+console.log(`${firstName} `.repeat(5));
+*/
+const years = [1990, 1965, 1982, 1937];
+//ES5
+var ages5 = years.map(function(el){
+    return 2019 - el;
+})
+console.log(ages5);
+//ES6
+let ages6 = years.map(el => 2019 - el);
+console.log(ages6);
+ages6 = years.map((el, index) => 2019 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index)=>{
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index+1}: ${age}`;
+})
+console.log(ages6);
