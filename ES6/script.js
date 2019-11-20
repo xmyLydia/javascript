194242e74bbe5ae3465ce5a6a8eb10cae7c41634
mxie <836728902@qq.com>
2019-11-20 22:27:48 +0200

    arrow function 2

--- ES6/script.js
+++ ES6/script.js
@@ -61,7 +61,7 @@ console.log(n.startsWith('H'));//start with
 console.log(n.endsWith('th'));//ends with
 console.log(n.includes('mi'));//if it is in the middle
 console.log(`${firstName} `.repeat(5));
-*/
+
 const years = [1990, 1965, 1982, 1937];
 //ES5
 var ages5 = years.map(function(el){
@@ -80,3 +80,72 @@ ages6 = years.map((el, index)=>{
     return `Age element ${index+1}: ${age}`;
 })
 console.log(ages6);
+*/
+//ES5
+var box5 = {
+    color: 'green',
+    position:1,
+    clickMe: function(){
+        var self = this;
+        document.querySelector('.green').addEventListener('click',
+                                                         function(){
+            var str = 'This is box number '+ self.position+
+                ' and color: '+self.color;
+            console.log(str);
+        })
+    }
+}
+box5.clickMe();
+//ES6
+const box6 = {
+    color: 'green',
+    position:1,
+    clickMe: function(){
+      
+        document.querySelector('.green').addEventListener('click',
+            ()=>{
+            var str = 'This is box number '+ this.position+
+                ' and color: '+this.color;
+            console.log(str);
+        })
+    }
+}
+//box6.clickMe();
+/*const box61 = {
+    color: 'green',
+    position:1,
+    clickMe: ()=>{
+      
+        document.querySelector('.green').addEventListener('click',
+            ()=>{
+            var str = 'This is box number '+ this.position+
+                ' and color: '+this.color;
+            console.log(str);
+        })
+    }
+}
+box61.clickMe();
+*/
+function Person(name){
+    this.name = name;
+}
+//ES5
+Person.prototype.myFriends5 = 
+    function(friends){
+    var arr = friends.map(function(el){
+        return this.name + 'is friends with '+el;
+    }.bind(this));
+    console.log(arr);
+}
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
+//ES6
+Person.prototype.myFriends6 = 
+    function(friends){
+    var arr = friends.map((el)=>{
+        return this.name + 'is friends with '+el;
+    } );
+    console.log(arr);
+}
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('Mike').myFriends6(friends);
