94dc8ce568332a88fd88a49a052d03edfe145d9d
mxie <836728902@qq.com>
2019-11-28 22:27:48 +0200

    first API call

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,4 +1,13 @@
-import str from './models/Search';
-//import{add, multiply, ID} from './views/searchView';
-import * as searchView from './views/searchView';
-console.log(` using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(searchView.ID, 2)} `);
+import axios from 'axios';
+
+async function getResults(query){
+
+    try{
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    }catch (errors){
+        alert(errors);
+    }
+}
+getResults('pasta');
